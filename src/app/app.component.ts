2864f68dcfb527174926467e67f10efd2988776f
CoutoDev <c.svitor@yahoo.com.br>
2021-02-04 00:01:22 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ciclo-vida';
+  ids: number[] = [];
+  lastId: number = 0;
+  label: string = '';
+
+  delete(id: number) {
+    this.ids.splice(this.ids.indexOf(id), 1);
+  }
+
+  add() {
+    this.lastId++;
+    this.ids.push(this.lastId);
+  }
 }
